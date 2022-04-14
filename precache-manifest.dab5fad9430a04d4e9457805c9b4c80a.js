81df633dd10ed809c385cf1fd3da535eb5387610
endhambot <endhambot@gmail.com>
2022-04-14 13:13:35 +0530

    Updates

---
+++ precache-manifest.dab5fad9430a04d4e9457805c9b4c80a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeabb9e70d19c5531bfc8d1d602f544",
+    "url": "/PeGuweatherapp/index.html"
+  },
+  {
+    "revision": "8d9fdeb8f6264f315bc2",
+    "url": "/PeGuweatherapp/static/css/main.da1f3f23.chunk.css"
+  },
+  {
+    "revision": "ae5fa3675b2f7bd99fbe",
+    "url": "/PeGuweatherapp/static/js/2.b11f209a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/PeGuweatherapp/static/js/2.b11f209a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9fdeb8f6264f315bc2",
+    "url": "/PeGuweatherapp/static/js/main.5ba50e9d.chunk.js"
+  },
+  {
+    "revision": "2f14b2ff20bc8fc1a5e3",
+    "url": "/PeGuweatherapp/static/js/runtime-main.003306c9.js"
+  },
+  {
+    "revision": "65c48b64f99407f5800f298fea27c56b",
+    "url": "/PeGuweatherapp/static/media/github.65c48b64.svg"
+  },
+  {
+    "revision": "5cea32b1f9b274a4fe2d10ed43b7c1db",
+    "url": "/PeGuweatherapp/static/media/left-arrow.5cea32b1.svg"
+  },
+  {
+    "revision": "ca2366a05dc0dd6d8cc3333839111d74",
+    "url": "/PeGuweatherapp/static/media/linkedin.ca2366a0.svg"
+  },
+  {
+    "revision": "75520b4a470b4916275654f3b1a15264",
+    "url": "/PeGuweatherapp/static/media/logo.75520b4a.svg"
+  },
+  {
+    "revision": "845e73a46e444a53fdba4f17b315ca27",
+    "url": "/PeGuweatherapp/static/media/right-arrow.845e73a4.svg"
+  },
+  {
+    "revision": "ec9e49da27e8b31efd89ff729c40c15c",
+    "url": "/PeGuweatherapp/static/media/search.ec9e49da.svg"
+  },
+  {
+    "revision": "c65a75179a4d11dca514eafda3e91a1e",
+    "url": "/PeGuweatherapp/static/media/telegram.c65a7517.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/PeGuweatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/PeGuweatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/PeGuweatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/PeGuweatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/PeGuweatherapp/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
